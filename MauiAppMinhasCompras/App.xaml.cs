ed8fdd4e222d977267de7a71aa832bd23f34aa54
Luan <Luan@DESKTOP-P2DHSF5>
2025-03-09 15:28:00 -0300

    edição de arquivos App.xaml, NovoProduto e ListaProduto

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,38 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        //declaração de membro privado (campo)
+        static SQLiteDatabaseHelper _db;
+        
+        //tornando o membro publico (propriedade)
+        public static SQLiteDatabaseHelper Db
+
+        {
+            // Este bloco descreve que sempre que
+            // eu chamar o Db eu tenho que dar um return no campo Db
+
+            get
+            {
+                //Verificando se já existe um objeto no campo Db, caso não tenha eu instancio e o objeto e retorno Db
+                if (_db == null)
+                {
+                    //encapsulamento para garantir que seja encontrado o caminho correto e também a indicação do diretório
+                    //onde o arquivo está armazenado
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
@@ -9,5 +40,7 @@
             //MainPage = new AppShell();
             MainPage = new NavigationPage(new Views.ListaProduto());
         }
+
     }
+
 }
--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,6 @@
+using MauiAppMinhasCompras.Models;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +9,26 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			//realizando o preenchimento da Model de Produto
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+			// realizando de forma assíncrona o método insert na Db
+			await App.Db.Insert(p);
+			//Avisando o usuário que o registro foi inserido
+			await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
 }
